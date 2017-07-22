0f9593b7a1cc6f36aae2b4e13caf604023c77299
SuganSubramani <smilysugan7.sk@gmail.com>
2017-07-22 16:44:59 +0530

    change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
 
   def update
   	@user = User.find(params[:id])
-  	if @user.update_attributes(params[:user])
+  	if @user.update_attributes(user_params)
   		redirect_to @user
   	else
   		render action: 'edit'
