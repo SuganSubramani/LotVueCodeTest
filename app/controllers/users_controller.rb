749407fbdeecbbf9a34c25160eed035e79152adb
SuganSubramani <smilysugan7.sk@gmail.com>
2017-08-24 12:40:48 +0530

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
 
   private
   	def user_params
-  		params.require(:user).permit(:first_name,:last_name,:email,:roles,role_ids: [])
+  		params.require(:user).permit(:first_name,:last_name,:email,:roles => [:id])
   	end
 end
